9f10b2cd3f316577cc9dc2c2f220b304a5fa43b5
Ellie Richardson-Jones <elliebethjones@gmail.com>
2020-02-17 15:48:42 +0000

    Add tests and code for divisible by three and five

--- spec/javaBuzzSpec.js
+++ spec/javaBuzzSpec.js
@@ -20,4 +20,40 @@ describe('Javabuzz', function() {
 
   });
 
+  describe('knows when a number is', function() {
+    
+    it('divisible by 5', function (){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(5)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by five', function() {
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByFive(3)).toBe(false);
+    });
+
+  });
+
+  describe('knows when a number is', function() {
+
+    it('divisble by 3 and 5', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThreeAndFive(15)).toBe(true);
+    });
+
+  });
+
+  describe('knows when a number is NOT', function() {
+
+    it('divisible by three and five', function(){
+      javabuzz = new Javabuzz();
+      expect(javabuzz.isDivisibleByThreeAndFive(16)).toBe(false);
+    });
+
+  });
+
 });
--- src/Javabuzz.js
+++ src/Javabuzz.js
@@ -3,3 +3,11 @@ var Javabuzz = function () {};
 Javabuzz.prototype.isDivisibleByThree = function(number) {
   return (number % 3 === 0);
 };
+
+Javabuzz.prototype.isDivisibleByFive = function(number) {
+  return (number % 5 === 0);
+};
+
+Javabuzz.prototype.isDivisibleByThreeAndFive = function(number) {
+  return (number % 3 === 0 && number % 5 === 0);
+};
